333702f69093ec82dd3830512ada8da4248164d9
fabia <fabia@192.168.56.1>
2023-11-20 20:15:17 -0600

    primera carga

---
+++ src/grupo2_avance_proyecto/categoria.java
@@ -0,0 +1,16 @@
+package grupo2_avance_proyecto;
+public class categoria {  
+    private String nombre_categoria;
+    
+    public categoria(String Nombre_categoria){
+        nombre_categoria=Nombre_categoria;
+    }
+    
+    public String getNombre_categoria(){
+        return nombre_categoria;
+    }
+    
+    public void setNombre_categoria(String Nombre_categoria){
+        nombre_categoria=Nombre_categoria;
+    }
+}
