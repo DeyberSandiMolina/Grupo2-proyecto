fa9d7e1c31d58f551573c8a3b350c6381a1c0ba0
fabia <fabia@192.168.56.1>
2023-11-20 20:15:26 -0600

    primera carga

---
+++ src/grupo2_avance_proyecto/marca.java
@@ -0,0 +1,19 @@
+
+package grupo2_avance_proyecto;
+
+public class marca {
+    private String nombre_marca;
+    
+    public marca(String Nombre_marca){
+        nombre_marca=Nombre_marca;
+    }
+    
+    public String getNombre_marca(){
+        return nombre_marca;
+    }
+    
+    public void setNombre_marca(String Nombre_marca){
+        nombre_marca=Nombre_marca;
+    }
+}
+
